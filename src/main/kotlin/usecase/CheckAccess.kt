e4e766ea83f89cbca5cdb1b4667b8bf3e96f8c41
jevtam <matvej.kukushkin@gmail.com>
2025-12-24 19:25:27 +0700

    fix

--- src/main/kotlin/usecase/CheckAccess.kt
+++ src/main/kotlin/usecase/CheckAccess.kt
@@ -4,7 +4,6 @@ import domain.model.Action
 import domain.services.AccessPolicy
 import domain.services.AuthService
 import domain.services.QuotaService
-import infrastructure.cli.CliParse
 
 data class CheckAccessInput(
     val login: String,
@@ -28,7 +27,7 @@ class CheckAccess(
     private val policy: AccessPolicy,
     private val quota: QuotaService
 ) {
-    fun execute(input: CliParse): CheckAccessResult {
+    fun execute(input: CheckAccessInput): CheckAccessResult {
         if (!auth.isPasswordValid(input.login, input.password))
             return CheckAccessResult.BadLoginOrPassword
 
